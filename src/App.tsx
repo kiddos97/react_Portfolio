82a05e3636cf77577f186122d863e35abe324b87
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-13 12:21:54 -0500

    adding images

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   useEffect(() => {
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <Hero />
       <About />
+      <Skills />
     </div>
   );
 }
