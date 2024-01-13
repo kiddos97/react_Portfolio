b2cccf53f59fc9f6535381a5424507145862aa1d
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-13 17:27:01 -0500

    adding projects

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Project from "./components/Project";
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Project />
     </div>
   );
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,17 @@
 const Project = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <div className="mx-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <div className="realtive flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]  "></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
