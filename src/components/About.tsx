66f3cfb8ccd4e0fa61d5f4f3db2b77332a529838
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-13 02:34:44 -0500

    updating about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
             velit nulla!
           </p>
           <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out some of my latets work
+            <a href="/"> Check out some of my latets work</a>
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
