26e42a2e903c341c89ea37aafa4e2da6ef73ffec
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-14 09:37:06 -0500

    updating

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+const Contact = () => {
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5] ">
+          Contact
+        </p>
+        <h2>Get in touch</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
