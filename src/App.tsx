da2fd1ba05cd063caa713196af213a5436e70df5
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-14 12:38:58 -0500

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
+//import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+//import Project_page from "./components/Project_page";
 
 function App() {
   useEffect(() => {
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,4 @@
+//import { Link } from "react-router-dom";
 import { Projects } from "../Project/Project";
 
 interface Props {
@@ -23,6 +24,7 @@ const ProjectList: React.FC<Props> = ({
         <p className="pb-4 pt-2 text-center font-bold text-white">
           {projectdescription}
         </p>
+
         <a href="/">
           <p className="text-center py-3 rounded-xl bg-white text-gray-700 font-bold cursor-pointer text-lg">
             {link}
