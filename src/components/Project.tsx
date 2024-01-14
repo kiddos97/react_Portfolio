8a5a1a009a1bbb0cff5eb4c3dabfcab7fdb3035d
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-14 08:54:01 -0500

    updating

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,26 +3,28 @@ import work from "../assets/workImg.jpeg";
 
 const Project = () => {
   return (
-    <div className="w-full">
-      <div className="mx-w-[1240px] mx-auto px-2 py-16">
+    <div className="w-full bg-[#ecf0f3]">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
         <h2 className="py-4">What I've built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="realtive flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
             <img
               className="rounded-xl group-hover:opacity-10"
               src={work}
               alt="/"
             />
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-              <h3 className="text-2xl font-bold tracking-wider text-center">
+              <h3 className="text-2xl font-bold tracking-wider text-center text-white">
                 Real Estate
               </h3>
-              <p className="pb-4 pt-2 text-center">React Js project</p>
+              <p className="pb-4 pt-2 text-center text-white">
+                React Js project
+              </p>
               <a
-                className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg"
+                className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg"
                 href="/"
               >
                 More Info
