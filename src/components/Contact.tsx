537c9f38aa507540356d59677fbf945aa3d83512
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-14 10:11:47 -0500

    adding contact and updating index.css

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -24,19 +24,19 @@ const Contact = () => {
               <div>
                 <h2 className="py-2">Emmanuel</h2>
                 <p>Front-End Developer</p>
-                <p className="py-4">
+                <p className="py-4 mb-4">
                   I am avaiable for free-lance positions. Contact me and let's
                   talk!
                 </p>
                 <div>
-                  <p className="uppercase pt-4"> Connect with me</p>
+                  <p className="uppercase text-center pt-8"> Connect with me</p>
                 </div>
-                <div className="py-4">
+                <div className="p-4">
                   <ul className="flex justify-center items-center">
-                    <li className="rounded-full shadow-xl shadow-gray-600 p-4 ml-4">
+                    <li className="rounded-full shadow-xl shadow-gray-600  p-4 ml-4">
                       <FaLinkedin style={{ color: "#5651e5" }} />
                     </li>
-                    <li className="rounded-full shadow-xl shadow-gray-600 p-4 ml-4">
+                    <li className="rounded-full shadow-xl shadow-gray-600  p-4 ml-4">
                       <FaGithub style={{ color: "#5651e5" }} />
                     </li>
                     <li className="rounded-full shadow-xl shadow-gray-600 p-4 ml-4">
@@ -51,6 +51,23 @@ const Contact = () => {
             </div>
           </div>
           {/* Right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-600 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form action="">
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase tetx-sm py-2" htmlFor="">
+                      Name
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex biorder-gray-300"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
