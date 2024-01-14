ab7444bacf2efd1a94c07a659c5d8b9f1815f811
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-14 09:12:58 -0500

    updating project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,7 +8,9 @@ const Project = () => {
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
-        <h2 className="py-4">What I've built</h2>
+        <h2 className="py-4 text-xl font-bold sm:text-4xl uppercase mb-4">
+          What I've built
+        </h2>
         <div className="grid md:grid-cols-2 gap-8">
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
             <img
@@ -16,18 +18,17 @@ const Project = () => {
               src={work}
               alt="/"
             />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
               <h3 className="text-2xl font-bold tracking-wider text-center text-white">
                 Real Estate
               </h3>
-              <p className="pb-4 pt-2 text-center text-white">
+              <p className="pb-4 pt-2 text-center font-bold text-white">
                 React Js project
               </p>
-              <a
-                className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg"
-                href="/"
-              >
-                More Info
+              <a href="/">
+                <p className="text-center py-3 rounded-xl bg-white text-gray-700 font-bold cursor-pointer text-lg">
+                  More Info
+                </p>
               </a>
             </div>
           </div>
