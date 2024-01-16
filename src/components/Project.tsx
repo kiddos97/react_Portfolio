d503f32707d2daccd6d44a548e68259f7b748e91
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-15 23:24:35 -0600

    updating package.json

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -15,7 +15,7 @@ const ProjectList: React.FC<Props> = ({
   link,
 }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-900 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <img className="rounded-xl group-hover:opacity-10" src={img} alt="/" />
       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
         <h3 className="text-2xl font-bold tracking-wider text-center text-white">
